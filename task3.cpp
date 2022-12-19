c1ce9bdf24877a43d2de5d18e5e070d913a5572d
786offiun <120181757+786offiun@users.noreply.github.com>
2022-12-19 10:37:40 -0800

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout <<"   ###"<<endl;
+cout <<"  #   #"<<endl;
+cout <<" ##   ##"<<endl;
+cout <<" #######"<<endl;
+cout <<" ##   ##"<<endl;
+cout <<" ##   ##"<<endl;
+cout <<" ##   ##"<<endl;
+    
+
+
+cout <<"       ___         ____    " <<endl;
+cout <<"         %         %    " <<endl;
+cout <<"          %       %      " <<endl;
+cout <<"           %     %       " <<endl;
+cout <<"            %   %         " <<endl;
+cout <<"             % %         " <<endl;
+cout  <<"            %%         " <<endl;
+cout <<"            %  %          " <<endl;
+cout <<"          %     %        " <<endl;
+cout << "      __%        %___   " <<endl;
+ 
+
+
+
+cout <<        "         |$$$$$$$$$$$$$$$$$$$$        "<<endl;
+cout <<        "                          **         "<<endl;
+cout <<         "                       ***      "<<endl;
+cout <<          "                 ****             "<<endl;
+cout <<           "              ***               "<<endl;
+cout <<    "            ***                        "<<endl;
+cout <<  "           &&$$$$$$$$$$$$$$$$$$$|                          "<<endl;
+ 
+
+
+
+cout <<           "       **           *     "<<endl;
+cout <<           "       **         *      "<<endl;
+cout <<           "       **       *        "<<endl;
+cout <<           "       **     *          "<<endl;
+cout <<           "       **   *             "<<endl;
+cout <<           "       ** *              "<<endl;
+cout <<           "       **   *             "<<endl;
+cout <<           "       **     *            "<<endl;
+cout <<           "       **       *        "<<endl;
+cout <<           "       **         *       "<<endl;
+cout <<           "       **           *       "<<endl;
+
+cout <<           "         ****|      "<<endl;
+cout <<           "        ***                "<<endl;
+cout <<           "       *              "<<endl;
+cout <<           "       .         "<<endl;
+cout <<           "        ***            "<<endl;
+cout <<           "           **        "<<endl;
+cout <<           "            **            "<<endl;
+cout <<           "       |    **        "<<endl;
+cout <<           "        *****     "<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
